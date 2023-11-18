002b65d157e1e3f695896812e25736ee16ca78ae
mmokijew <marek.mokijewski@capgemini.com>
2023-11-18 16:00:14 +0100

    initial commit from spring boot initializer

---
+++ src/main/java/com/mmokijewski/bikeRentalApp/BikeRentalAppApplication.java
@@ -0,0 +1,13 @@
+package com.mmokijewski.bikeRentalApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeRentalAppApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(BikeRentalAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mmokijewski/bikeRentalApp/BikeRentalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmokijewski.bikeRentalApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeRentalAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
